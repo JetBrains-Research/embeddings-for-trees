34a9c115457c92548776e81ee9723c7c65806111
Spirin Egor <egor@spirin.tech>
2019-10-17 20:09:55 +0300

    add converting functions for getting dgl representations of ast

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -3,9 +3,15 @@ from argparse import ArgumentParser, Namespace
 from shutil import rmtree
 from tarfile import open as tar_open
 from subprocess import run as subprocess_run
-from typing import Tuple
+from typing import Tuple, Dict
+from pickle import dump as pkl_dump
+from collections import Counter
+from multiprocessing import Pool, cpu_count
 from requests import get
 from tqdm import tqdm
+from networkx.drawing.nx_pydot import read_dot
+from dgl import DGLGraph
+import pandas as pd
 
 data_folder = 'data'
 holdout_folders = ['training', 'validation', 'test']
@@ -55,9 +61,9 @@ def extract_dataset(file_path: str, extract_path: str, folder_name: str) -> [str
 
 def build_project_asts(project_path: str, output_path: str) -> bool:
     completed_process = subprocess_run(
-        ['java', '-jar', astminer_cli_path, 'parse',
+        ['java', '-Xmx15g', '-jar', astminer_cli_path, 'parse',
          '--project', project_path, '--output', output_path,
-         '--storage', 'dot', '--granularity', 'method', '--lang', 'java']
+         '--storage', 'dot', '--granularity', 'method', '--lang', 'java', '--hide-method-name']
     )
     if completed_process.returncode != 0:
         print(f"can't build ASTs for project {project_path}, failed with:\n{completed_process.stdout}")
@@ -65,7 +71,7 @@ def build_project_asts(project_path: str, output_path: str) -> bool:
     return True
 
 
-def build_holdout_asts(data_path: str, holdout_name: str) -> Tuple[int, int]:
+def build_holdout_asts(data_path: str, holdout_name: str) -> str:
     print(f"build asts for {holdout_name} data...")
     projects = os.listdir(os.path.join(data_path, holdout_name))
     output_folder_path = os.path.join(data_path, f'{holdout_name}_asts')
@@ -77,10 +83,82 @@ def build_holdout_asts(data_path: str, holdout_name: str) -> Tuple[int, int]:
         create_folder(output_project_path)
         if build_project_asts(project_path, output_project_path):
             successful_builds += 1
-    return successful_builds, len(projects)
+    print(f"create asts for {successful_builds}/{len(projects)} {holdout_name} projects")
+    return output_folder_path
 
 
-def main(args: Namespace):
+def convert_ast(ast_path: str, output_path: str, ast_description: pd.DataFrame) -> None:
+    g_nx = read_dot(ast_path)
+    g_dgl = DGLGraph(g_nx)
+    g_dgl.ndata['token'] = ast_description['token_id'].values
+    g_dgl.ndata['type'] = ast_description['type_id'].values
+    label = ast_description['label'].values[0]
+    with open(output_path, 'wb') as file_out:
+        pkl_dump({'ast': g_dgl, 'method_name': label}, file_out)
+
+
+def convert_project(
+        holdout_asts_path: str, holdout_output_path: str, project_name: str,
+        token_to_id: Dict, type_to_id: Dict, n_jobs: int = -1
+) -> None:
+    project_path = os.path.join(holdout_asts_path, project_name, 'java')
+    ast_folder = os.path.join(project_path, 'asts')
+    description = pd.read_csv(os.path.join(project_path, 'description.csv'))
+    description['token'].fillna(value='NAN', inplace=True)
+    description['token_id'] = description['token'].apply(lambda token: token_to_id.get(token, 1))
+    description['type_id'] = description['type'].apply(lambda type_all: type_to_id.get(type, 0))
+    asts = os.listdir(ast_folder)
+    pool = Pool(cpu_count() if n_jobs == -1 else n_jobs)
+    print(f"working with {project_name}...")
+    pool.starmap_async(
+        convert_ast,
+        [(os.path.join(project_path, 'asts', ast),
+          os.path.join(holdout_output_path, f'{project_name}_{ast[:-4]}.pkl'),
+          description[description['dot_file'] == ast].sort_values(by='node_id')) for ast in tqdm(asts)]
+    )
+    pool.close()
+
+
+def convert_holdout(data_path: str, holdout_name: str, token_to_id: Dict, type_to_id: Dict, n_jobs: int = -1) -> str:
+    print(f"Convert asts for {holdout_name} data...")
+    holdout_path = os.path.join(data_path, f'{holdout_name}_asts')
+    output_holdout_path = os.path.join(data_path, f'{holdout_name}_preprocessed')
+    create_folder(output_holdout_path)
+    projects = os.listdir(holdout_path)
+    for project in tqdm(projects):
+        convert_project(holdout_path, output_holdout_path, project, token_to_id, type_to_id, n_jobs)
+    return output_holdout_path
+
+
+def collect_vocabulary(train_path: str, n_most_common_tokens: int = 1_000_000) -> Tuple[Dict, Dict]:
+    token_vocabulary = Counter()
+    type_vocabulary = Counter()
+    projects = os.listdir(train_path)
+    print("collect vocabulary from training holdout")
+    for project in tqdm(projects):
+        project_description = pd.read_csv(os.path.join(train_path, project, 'java', 'description.csv'))
+        token_vocabulary.update(project_description['token'].values)
+        type_vocabulary.update(project_description['type'].values)
+    del token_vocabulary['METHOD_NAME']
+    del token_vocabulary['NAN']
+    print(f"found {len(token_vocabulary)} tokens, using {n_most_common_tokens} from it")
+    print(f"found {len(type_vocabulary)} types, using all of them")
+    token_to_id = {
+        'UNK': 0,
+        'METHOD_NAME': 1,
+        'NAN': 2
+    }
+    token_to_id.update(
+        [(token[0], num + 3) for num, token in enumerate(token_vocabulary.most_common(n_most_common_tokens))]
+    )
+    type_to_id = {
+        'UNK': 0
+    }
+    type_to_id.update([(node_type[0], num + 1) for num, node_type in enumerate(type_vocabulary)])
+    return token_to_id, type_to_id
+
+
+def main(args: Namespace) -> None:
     dataset_name = dataset_mapping[args.dataset]
     data_path = os.path.join(data_folder, dataset_name)
 
@@ -98,18 +176,35 @@ def main(args: Namespace):
     if not all([os.path.exists(holdout_path) for holdout_path in [train_path, val_path, test_path]]):
         raise RuntimeError("download and extract data before processing it via --download arg")
 
-    train_projects = os.listdir(train_path)
-    val_projects = os.listdir(val_path)
-    test_projects = os.listdir(test_path)
-    print(f"found {len(train_projects)} train projects, " +
-          f"{len(val_projects)} validation projects, {len(test_projects)} test projects")
-
     if args.build_ast:
         if not os.path.exists(astminer_cli_path):
             raise RuntimeError(f"can't find astminer-cli in this location {astminer_cli_path}")
+        holdout_ast_paths = {}
         for holdout in holdout_folders:
-            success, total = build_holdout_asts(data_path, holdout)
-            print(f"create asts for {success}/{total} {holdout} projects")
+            holdout_ast_paths[holdout] = build_holdout_asts(data_path, holdout)
+    else:
+        holdout_ast_paths = {
+            holdout: os.path.join(data_path, f'{holdout}_asts') for holdout in holdout_folders
+        }
+
+    if not all([os.path.exists(path[1]) for path in holdout_ast_paths.items()]):
+        raise RuntimeError("build ast before converting it via --build_ast arg")
+
+    if args.convert:
+        token_to_id, type_to_id = collect_vocabulary(os.path.join(data_path, f'{holdout_folders[0]}_asts'))
+        holdout_preprocessed_paths = {}
+        for holdout in holdout_folders:
+            holdout_preprocessed_paths[holdout] = convert_holdout(
+                data_path, holdout, token_to_id, type_to_id, args.n_jobs
+            )
+    else:
+        holdout_preprocessed_paths = {
+            holdout: os.path.join(data_path, f'{holdout}_preprocessed') for holdout in holdout_folders
+        }
+
+    for holdout, path in holdout_preprocessed_paths.items():
+        number_of_functions = len(os.listdir(path))
+        print(f"There are {number_of_functions} functions in {holdout} data")
 
 
 if __name__ == '__main__':
@@ -117,5 +212,7 @@ if __name__ == '__main__':
     arg_parser.add_argument('dataset', choices=list(dataset_mapping.keys()))
     arg_parser.add_argument('--download', action='store_true')
     arg_parser.add_argument('--build_ast', action='store_true')
+    arg_parser.add_argument('--convert', action='store_true')
+    arg_parser.add_argument('--n-jobs', type=int, default=-1)
 
     main(arg_parser.parse_args())
