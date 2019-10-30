d62c49987ceb881ff681a0f4d7c9b814a310fcda
Spirin Egor <egor@spirin.tech>
2019-10-30 21:52:24 +0300

    fix dtype of mask tensor

--- train.py
+++ train.py
@@ -54,7 +54,7 @@ def train(params: Dict) -> None:
         running_loss = 0.0
         for step, (graph, labels) in tqdm(enumerate(training_set)):
             torch_labels = torch.tensor([label_to_id.get(label, 0) for label in labels]).to(device)
-            mask = torch.zeros(graph.number_of_nodes())
+            mask = torch.zeros(graph.number_of_nodes(), dtype=torch.bool)
             idx_of_roots = cumsum([0] + graph.batch_num_nodes)[:-1]
             mask[idx_of_roots] = 1
 
