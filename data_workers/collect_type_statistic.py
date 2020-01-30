3e060fdc73fe3036f651bccb0eac578a3371198e
Spirin Egor <spirin.egor@mail.ru>
2020-01-30 19:10:52 +0300

    parallelize collecting statistic

--- data_workers/collect_type_statistic.py
+++ data_workers/collect_type_statistic.py
@@ -1,7 +1,8 @@
 import os
 import pickle
 from argparse import ArgumentParser
-from os.path import join
+from multiprocessing import Pool, cpu_count
+from typing import Tuple
 
 import dgl
 import numpy as np
@@ -9,31 +10,48 @@ import pandas as pd
 from tqdm.auto import tqdm
 
 
-def collect_type_statistic(data_folder: str, vocabulary: str) -> pd.DataFrame:
-    with open(vocabulary, 'rb') as pkl_file:
-        type_to_id = pickle.load(pkl_file)['type_to_id']
-    id_to_type = {v: k for k, v in type_to_id.items()}
+def _process_batch(arg: Tuple[str, int]) -> pd.DataFrame:
+    batch_path, number_of_types = arg
+    with open(batch_path, 'rb') as pkl_file:
+        graphs = dgl.unbatch(
+            pickle.load(pkl_file)['batched_graph']
+        )
     stats = pd.DataFrame(
         columns=['count'], dtype=np.int
     )
+    for graph in graphs:
+        for n_i in range(graph.number_of_nodes()):
+            cur_type_id = graph.ndata['type_id'][n_i].item()
+            children_mask_int = [0 for _ in range(number_of_types)]
+            for child_type_id in graph.ndata['type_id'][graph.out_edges(n_i)[1]]:
+                children_mask_int[child_type_id.item()] += 1
+            children_mask = ''.join(map(str, children_mask_int))
+            pandas_index = pd.MultiIndex.from_tuples([(cur_type_id, children_mask)])
+            if (cur_type_id, children_mask) not in stats.index:
+                new_row = pd.DataFrame({'count': [0]}, index=pandas_index)
+                stats = stats.append(new_row)
+            stats.loc[pandas_index, 'count'] += 1
+    return stats
+
+
+def collect_type_statistic(data_folder: str, vocabulary: str, n_jobs: int = -1) -> pd.DataFrame:
+    with open(vocabulary, 'rb') as pkl_file:
+        type_to_id = pickle.load(pkl_file)['type_to_id']
+    id_to_type = {v: k for k, v in type_to_id.items()}
+
     batches = os.listdir(data_folder)
-    for batch in tqdm(batches):
-        with open(join(data_folder, batch), 'rb') as pkl_file:
-            graphs = dgl.unbatch(
-                pickle.load(pkl_file)['batched_graph']
-            )
-        for graph in tqdm(graphs):
-            for n_i in range(graph.number_of_nodes()):
-                cur_type_id = graph.ndata['type_id'][n_i].item()
-                children_mask_int = [0 for _ in range(len(type_to_id))]
-                for child_type_id in graph.ndata['type_id'][graph.out_edges(n_i)[1]]:
-                    children_mask_int[child_type_id.item()] += 1
-                children_mask = ''.join(map(str, children_mask_int))
-                pandas_index = pd.MultiIndex.from_tuples([(cur_type_id, children_mask)])
-                if (cur_type_id, children_mask) not in stats.index:
-                    new_row = pd.DataFrame({'count': [0]}, index=pandas_index)
-                    stats = stats.append(new_row)
-                stats.loc[pandas_index, 'count'] += 1
+    if n_jobs == -1:
+        n_jobs = cpu_count()
+    with Pool(n_jobs) as pool:
+        results = pool.imap(
+            _process_batch,
+            map(lambda _p: (os.path.join(data_folder, _p), len(type_to_id)), batches)
+        )
+        stats_per_batch = list(tqdm(results, total=len(batches)))
+    stats = pd.concat(
+        [_s.reset_index() for _s in stats_per_batch]
+    )
+    stats = stats.groupby('index').agg('sum')
     stats = stats.reset_index()
     stats['node_type_id'] = stats['index'].apply(lambda t: t[0])
     stats['node_type'] = stats['node_type_id'].apply(lambda t: id_to_type[t])
