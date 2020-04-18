da993e39ce4af3c2d4c7a3bd65204d4ed45e92bb
Spirin Egor <spirin.egor@mail.ru>
2020-04-18 20:23:54 +0300

    add general luong attention

--- model/attention.py
+++ model/attention.py
@@ -14,11 +14,11 @@ class _IAttention(nn.Module):
         self.h_enc = h_enc
         self.h_dec = h_dec
 
-    def forward(self, prev_hidden_states: torch.Tensor, encoder_output: torch.Tensor, tree_sizes: List)\
+    def forward(self, hidden_states: torch.Tensor, encoder_output: torch.Tensor, tree_sizes: List)\
             -> torch.Tensor:
         """ Compute attention weights based on previous decoder state and encoder output
 
-        :param prev_hidden_states: [batch size, hidden size]
+        :param hidden_states: [batch size, hidden size]
         :param encoder_output: [number of nodes in batch, hidden size]
         :param tree_sizes: [batch size]
         :return: attention weights [number of nodes in batch, 1]
@@ -26,28 +26,41 @@ class _IAttention(nn.Module):
         raise NotImplementedError
 
 
-class LuongConcatAttention(_IAttention):
-    def __init__(self, h_enc: int, h_dec: int) -> None:
+class LuongAttention(_IAttention):
+    def __init__(self, h_enc: int, h_dec: int, score: str = 'concat') -> None:
         super().__init__(h_enc, h_dec)
-        self.linear = nn.Linear(self.h_dec + self.h_enc, self.h_enc, bias=False)
-        self.v = nn.Parameter(torch.rand(self.h_enc, 1), requires_grad=True)
+        self.score = score
+        if self.score == 'concat':
+            self.linear = nn.Linear(self.h_dec + self.h_enc, self.h_enc, bias=False)
+            self.v = nn.Parameter(torch.rand(self.h_enc, 1), requires_grad=True)
+        elif self.score == 'general':
+            self.linear = nn.Linear(self.h_enc, h_dec, bias=False)
+        else:
+            raise ValueError(f"Unknown score function: {score}")
 
-    def forward(self, prev_hidden_states: torch.Tensor, encoder_output: torch.Tensor, tree_sizes: List)\
+    def forward(self, hidden_states: torch.Tensor, encoder_output: torch.Tensor, tree_sizes: List)\
             -> torch.Tensor:
         # [number of nodes in batch, h_dec]
         repeated_hidden_states = torch.cat(
             [prev_hidden_state.expand(tree_size, -1)
-             for prev_hidden_state, tree_size in zip(prev_hidden_states, tree_sizes)],
+             for prev_hidden_state, tree_size in zip(hidden_states, tree_sizes)],
             dim=0
         )
 
-        # [number of nodes in batch, h_enc]
-        energy = torch.tanh(self.linear(
-            torch.cat((repeated_hidden_states, encoder_output), dim=1)
-        ))
-
-        # [number of nodes in batch, 1]
-        scores = torch.matmul(energy, self.v)
+        if self.score == 'concat':
+            # [number of nodes in batch, h_enc]
+            energy = torch.tanh(self.linear(
+                torch.cat((repeated_hidden_states, encoder_output), dim=1)
+            ))
+            # [number of nodes in batch, 1]
+            scores = torch.matmul(energy, self.v)
+        elif self.score == 'general':
+            # [number of nodes in batch; h dec]
+            linear_encoder = self.linear(encoder_output)
+            # [number of nodes in batch; 1]
+            scores = torch.bmm(linear_encoder.unsqueeze(1), repeated_hidden_states.unsqueeze(2)).squeeze(2)
+        else:
+            raise RuntimeError("Oh, this is strange")
 
         # [number of nodes in batch, 1]
         attentions = torch.cat(
@@ -75,7 +88,7 @@ def scaled_dot_product_attention(
 
 def get_attention(attention: str) -> _IAttention:
     attentions = {
-        LuongConcatAttention.__name__: LuongConcatAttention
+        LuongAttention.__name__: LuongAttention
     }
     if attention not in attentions:
         raise ValueError(f"unknown attention mechanism: {attention}")
