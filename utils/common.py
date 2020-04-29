053c0180fd07283ed5f358917ded3e858f1ebca0
Spirin Egor <spirin.egor@mail.ru>
2020-04-29 11:38:23 +0300

    add more fixing seed

--- utils/common.py
+++ utils/common.py
@@ -34,6 +34,7 @@ def fix_seed(seed: int = 7) -> None:
     torch.manual_seed(seed)
     dgl.random.seed(seed)
     if torch.cuda.is_available():
+        torch.cuda.manual_seed(seed)
         torch.cuda.manual_seed_all(seed)
         torch.backends.cudnn.deterministic = True
         torch.backends.cudnn.benchmark = False
