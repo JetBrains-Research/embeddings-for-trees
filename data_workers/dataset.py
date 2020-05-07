6f96c006720ef374056d2b423a3631b7b74def45
Spirin Egor <spirin.egor@mail.ru>
2020-05-07 17:51:25 +0300

    adapt dataset to new data format

--- data_workers/dataset.py
+++ data_workers/dataset.py
@@ -1,13 +1,21 @@
 import os
-from typing import Tuple
+from pickle import load
+from typing import Tuple, List
 
 import torch
 from dgl import DGLGraph, batch
-from dgl.data.utils import load_labels, load_graphs
+from dgl.data.utils import load_graphs
 from torch.utils.data import Dataset
 from tqdm.auto import tqdm
 
 from utils.common import get_tree_depth
+
+
+def _get_batches(path: str, ext: str) -> List[str]:
+    files = filter(lambda _f: _f.endswith(f'.{ext}'), os.listdir(path))
+    # assuming filenames are "batch_xxx.{ext}"
+    files = sorted(files, key=lambda name: int(name[6:-len(ext) - 1]))
+    return [os.path.join(path, gf) for gf in files]
 
 
 class JavaDataset(Dataset):
@@ -16,21 +24,19 @@ class JavaDataset(Dataset):
             self, dataset_path: str, batch_size: int, device: torch.device, invert_edges: bool = False
     ) -> None:
         assert os.path.exists(dataset_path)
-
         self.device = device
         self.invert_edges = invert_edges
-
-        graph_files = filter(lambda _f: _f.endswith('.dgl'), os.listdir(dataset_path))
-        graph_files = sorted(graph_files, key=lambda name: int(name[6:-4]))
-        graph_files = [os.path.join(dataset_path, gf) for gf in graph_files]
-
+        label_files = _get_batches(dataset_path, 'pkl')
+        graph_files = _get_batches(dataset_path, 'dgl')
         self.batch_description = []
 
         # iterate over pkl files to aggregate information about batches
         print(f"prepare the {dataset_path} dataset...")
-        for graph_file in tqdm(graph_files):
-            labels = load_labels(graph_file)
-            n_graphs = len(labels['labels'])
+        for graph_file, label_file in tqdm(zip(graph_files, label_files), total=len(graph_files)):
+            with open(label_file, 'rb') as pkl_file:
+                pkl_data = load(pkl_file)
+                labels = pkl_data['labels']
+            n_graphs = len(labels)
 
             batches_per_file = n_graphs // batch_size + (1 if n_graphs % batch_size > 0 else 0)
 
@@ -38,15 +44,24 @@ class JavaDataset(Dataset):
             for batch_id in range(batches_per_file):
                 start_index = batch_id * batch_size
                 end_index = min(n_graphs, (batch_id + 1) * batch_size)
-                self.batch_description.append((graph_file, start_index, end_index))
+                self.batch_description.append((graph_file, label_file, start_index, end_index))
+
+        self.current_label_file = None
+        self.labels = None
 
     def __len__(self) -> int:
         return len(self.batch_description)
 
     def __getitem__(self, item) -> Tuple[DGLGraph, torch.Tensor]:
-        graph_filename, start_index, end_index = self.batch_description[item]
+        graph_filename, label_filename, start_index, end_index = self.batch_description[item]
 
-        graphs, label_dict = load_graphs(graph_filename, list(range(start_index, end_index)))
+        if label_filename != self.current_label_file:
+            self.current_label_file = label_filename
+            with open(label_filename, 'rb') as pkl_file:
+                pkl_data = load(pkl_file)
+            self.labels = torch.tensor(pkl_data['labels'].T, device=self.device).detach()
+
+        graphs, _ = load_graphs(graph_filename, list(range(start_index, end_index)))
         graphs, mask = zip(*[
             (g, i) for i, g in enumerate(graphs)
             if g.number_of_nodes() < 10000 and get_tree_depth(g) < 100
@@ -59,7 +74,6 @@ class JavaDataset(Dataset):
         graph.ndata['token'] = graph.ndata['token'].to(self.device).detach()
         graph.ndata['type'] = graph.ndata['type'].to(self.device).detach()
         # [sequence len, batch size]
-        labels = label_dict['labels'][start_index:end_index, :][list(mask)]
-        labels = labels.t().to(self.device).detach()
+        labels = self.labels[:, start_index:end_index][:, list(mask)]
 
         return graph, labels
