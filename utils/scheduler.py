dca6a5a727b0efeb1744ae1379ae706e38e58cea
Spirin Egor <spirin.egor@mail.ru>
2020-05-09 17:34:19 +0300

    add const scheduler, refactor get scheduler function

--- utils/scheduler.py
+++ utils/scheduler.py
@@ -4,28 +4,24 @@ import numpy
 import torch
 
 
-def get_scheduler(scheduler_info: Dict, optimizer, total_steps: int):
-    name = scheduler_info.get('name', '')
-    if name == 'step':
+def get_scheduler(params: Dict, optimizer, total_steps: int):
+    name = params.get('name', '')
+    if name == 'const':
+        scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=1, gamma=1)
+    elif name == 'step':
         scheduler = torch.optim.lr_scheduler.StepLR(
-            optimizer, step_size=scheduler_info['size'], gamma=scheduler_info['gamma']
+            optimizer, step_size=params['step'], gamma=params['gamma']
         )
-    # LambdaLR multiply lr by function result
-    # setting initial lr to 1.0 correspond to regularize lr throw lambda
-    elif name == 'vaswani':
+    elif name in ['vaswani', 'cosine']:
+        # LambdaLR multiply original lr to lambda function result
+        # If original lr is equal to 1, it's correspond to control it by function
         for g in optimizer.param_groups:
             g['lr'] = 1
-        scheduler = torch.optim.lr_scheduler.LambdaLR(
-            optimizer, lr_lambda=_vaswani_lambda(scheduler_info['warm_up_cf'], scheduler_info['d_model'], total_steps)
-        )
-    elif name == 'cosine_warm_up':
-        for g in optimizer.param_groups:
-            g['lr'] = 1
-        scheduler = torch.optim.lr_scheduler.LambdaLR(
-            optimizer, lr_lambda=_cosine_warm_up_lambda(
-                scheduler_info['warm_up_cf'], scheduler_info['max_lr'], scheduler_info['min_lr'], total_steps
-            )
-        )
+        scheduler_lambda = \
+            _vaswani_lambda(params['warm_up_cf'], params['d_model'], total_steps) \
+            if name == 'vaswani' else \
+            _cosine_lambda(params['warm_up_cf'], params['max_lr'], params['min_lr'], total_steps)
+        scheduler = torch.optim.lr_scheduler.LambdaLR(optimizer, lr_lambda=scheduler_lambda)
     else:
         raise ValueError(f"unknown scheduler name: {name}")
 
@@ -40,7 +36,7 @@ def _vaswani_lambda(warm_up_cf: float, d_model: int, total_steps: int) -> Callab
     return lambda cur_step: d_model_cf * min(numpy.power(cur_step + 1, -0.5), warm_start_cf * (cur_step + 1))
 
 
-def _cosine_warm_up_lambda(warm_up_cf: float, max_lr: float, min_lr: float, total_steps: int) -> Callable[[int], float]:
+def _cosine_lambda(warm_up_cf: float, max_lr: float, min_lr: float, total_steps: int) -> Callable[[int], float]:
     warm_up_steps = int(warm_up_cf * total_steps)
     return lambda cur_step: \
         min_lr + (max_lr - min_lr) * cur_step / warm_up_steps \
