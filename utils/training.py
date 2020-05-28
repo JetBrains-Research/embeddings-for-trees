29ce04670c44291062dee372049403f6be7673ad
Spirin Egor <spirin.egor@mail.ru>
2020-05-28 13:21:03 +0300

    adopt forward pass for linear decoder

--- utils/training.py
+++ utils/training.py
@@ -21,7 +21,7 @@ def _forward_pass(
 
     :param model: Tree2Seq model
     :param graph: batched dgl graph
-    :param labels: [sequence len, batch size] ground truth labels
+    :param labels: [seq len; batch size] ground truth labels
     :param criterion: criterion to optimize
     :param device: torch device
     :return: Tuple[
@@ -30,27 +30,34 @@ def _forward_pass(
         batch info [Dict] dict with statistics
     ]
     """
+    # [seq len; batch size; vocab size]
     root_logits = model(
         graph,
         torch.tensor(get_root_indexes(graph.batch_num_nodes), dtype=torch.long, device=device, requires_grad=False),
         labels, device
     )
-    # remove <SOS> token
-    # [the longest sequence, batch size, vocab size]
-    root_logits = root_logits[1:]
-    # [the longest sequence, batch size]
-    labels = labels[1:]
+
+    # if seq len in labels equal to 1, then model solve classification task
+    # for longer sequences we should remove <SOS> token, since it's always on the first place
+    if labels.shape[0] > 1:
+        # [seq len - 1; batch size; vocab size]
+        root_logits = root_logits[1:]
+        # [seq len - 1; batch size]
+        labels = labels[1:]
 
     loss = criterion(root_logits.reshape(-1, root_logits.shape[-1]), labels.reshape(-1))
     # [the longest sequence, batch size]
     prediction = model.predict(root_logits)
 
     # Calculate metrics
+    skipping_tokens = [model.decoder.label_to_id[token]
+                       for token in [PAD, UNK, EOS]
+                       if token in model.decoder.label_to_id]
     batch_info = {
         'loss': loss.item(),
         'statistics':
             calculate_batch_statistics(
-                labels.t(), prediction.t(), [model.decoder.label_to_id[token] for token in [PAD, UNK, EOS]]
+                labels.t(), prediction.t(), skipping_tokens
             )
     }
 
