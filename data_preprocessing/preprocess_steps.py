4133af7dd3ec82da4ac1b1a324a69d8be97be06e
Spirin Egor <spirin.egor@mail.ru>
2020-06-16 14:01:29 +0300

    fix data extracting

--- data_preprocessing/preprocess_steps.py
+++ data_preprocessing/preprocess_steps.py
@@ -32,7 +32,7 @@ def download_dataset(dataset_info: IDatasetInfo, dataset_path: str) -> None:
     print(f"download {dataset_info.name} dataset...")
     tar_file_path = _download_dataset_archive(dataset_info, dataset_path)
     print(f"extract files from tar archive {tar_file_path}...")
-    extract_tar_gz(tar_file_path, dataset_path)
+    extract_tar_gz(tar_file_path, os.path.dirname(dataset_path))
     print("remove tar file...")
     os.remove(tar_file_path)
 
