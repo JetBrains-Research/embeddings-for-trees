c8167d87b3bb94522e44c82657d3e22dd7c10c85
Egor Spirin <spirin.egor@mail.ru>
2021-02-12 18:30:33 +0300

    Add java full dataset support

--- data_module/jsonl_data_module.py
+++ data_module/jsonl_data_module.py
@@ -21,6 +21,7 @@ class JsonlDataModule(LightningDataModule):
     _known_datasets = {
         "java-test-asts": "https://s3-eu-west-1.amazonaws.com/datasets.ml.labs.aws.intellij.net/java-ast-methods/java-test-asts.tar.gz",
         "java-small-asts": "https://s3-eu-west-1.amazonaws.com/datasets.ml.labs.aws.intellij.net/java-ast-methods/java-small-asts.tar.gz",
+        "java-full-asts": "https://s3-eu-west-1.amazonaws.com/datasets.ml.labs.aws.intellij.net/code-summarization/java-full-asts.tar.gz",
     }
 
     def __init__(self, config: DictConfig):
