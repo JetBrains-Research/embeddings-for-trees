7359922a8906e3396ac5e0cc790572253e05ac6d
Egor Spirin <spirin.egor@mail.ru>
2021-08-09 15:26:08 +0500

    Return original implementation

--- embeddings_for_trees/models/modules/tree_lstm_encoder.py
+++ embeddings_for_trees/models/modules/tree_lstm_encoder.py
@@ -2,61 +2,70 @@ from typing import Dict
 
 import dgl
 import torch
-from dgl.udf import EdgeBatch, NodeBatch
 from omegaconf import DictConfig
 from torch import nn
-
-
-class ChildSumTreeLSTMCell(nn.Module):
-    def __init__(self, x_size: int, h_size: int):
-        super(ChildSumTreeLSTMCell, self).__init__()
-        self.W_iou = nn.Linear(x_size, 3 * h_size, bias=False)
-        self.U_iou = nn.Linear(h_size, 3 * h_size, bias=False)
-        self.b_iou = nn.Parameter(torch.zeros(1, 3 * h_size))
-        self.U_f = nn.Linear(h_size, h_size)
-
-    def message_func(self, edges: EdgeBatch):
-        return {"h": edges.src["h"], "c": edges.src["c"]}
-
-    def reduce_func(self, nodes: NodeBatch):
-        h_tild = torch.sum(nodes.mailbox["h"], 1)
-        f = torch.sigmoid(self.U_f(nodes.mailbox["h"]))
-        c = torch.sum(f * nodes.mailbox["c"], 1)
-        return {"iou": self.U_iou(h_tild), "c": c}
-
-    def apply_node_func(self, nodes: NodeBatch):
-        iou = nodes.data["iou"] + self.b_iou
-        i, o, u = torch.chunk(iou, 3, 1)
-        i, o, u = torch.sigmoid(i), torch.sigmoid(o), torch.tanh(u)
-        c = i * u + nodes.data["c"]
-        h = o * torch.tanh(c)
-        return {"h": h, "c": c}
 
 
 class TreeLSTM(nn.Module):
     def __init__(self, config: DictConfig):
         super().__init__()
         self._encoder_size = config.encoder_size
+
         self._dropout = nn.Dropout(config.encoder_dropout)
-        self._linear = nn.Linear(config.encoder_size, config.decoder_size)
-        self._cell = ChildSumTreeLSTMCell(config.embedding_size, config.encoder_size)
+
+        self._W_iou = nn.Linear(config.embedding_size, 3 * config.encoder_size)
+        self._U_iou = nn.Linear(config.encoder_size, 3 * config.encoder_size, bias=False)
+
+        self._W_f = nn.Linear(config.embedding_size, config.encoder_size)
+        self._U_f = nn.Linear(config.encoder_size, config.encoder_size, bias=False)
+
+        self._out_linear = nn.Linear(config.encoder_size, config.decoder_size)
+        self._norm = nn.LayerNorm(config.decoder_size)
+        self._tanh = nn.Tanh()
+
+    def message_func(self, edges: dgl.udf.EdgeBatch) -> Dict:
+        h_f = self._U_f(edges.src["h"])
+        x_f = edges.dst["x_f"]
+        f = torch.sigmoid(x_f + h_f)
+        return {"Uh": self._U_iou(edges.src["h"]), "fc": edges.src["c"] * f}
+
+    @staticmethod
+    def reduce_func(nodes: dgl.udf.NodeBatch) -> Dict:
+        return {"Uh_sum": torch.sum(nodes.mailbox["Uh"], dim=1), "fc_sum": torch.sum(nodes.mailbox["fc"], dim=1)}
+
+    @staticmethod
+    def apply_node_func(nodes: dgl.udf.NodeBatch) -> Dict:
+        iou = nodes.data["x_iou"] + nodes.data["Uh_sum"]
+        i, o, u = torch.chunk(iou, 3, 1)
+        i, o, u = torch.sigmoid(i), torch.sigmoid(o), torch.tanh(u)
+
+        c = i * u + nodes.data["fc_sum"]
+        h = o * torch.tanh(c)
+
+        return {"h": h, "c": c}
 
     def forward(self, graph: dgl.DGLGraph) -> torch.Tensor:
         x = self._dropout(graph.ndata["x"])
 
         # init matrices for message propagation
         number_of_nodes = graph.number_of_nodes()
-        graph.ndata["iou"] = self._cell.W_iou(x)
+        graph.ndata["x_iou"] = self._W_iou(x)
+        graph.ndata["x_f"] = self._W_f(x)
         graph.ndata["h"] = graph.ndata["x"].new_zeros((number_of_nodes, self._encoder_size))
         graph.ndata["c"] = graph.ndata["x"].new_zeros((number_of_nodes, self._encoder_size))
+        graph.ndata["Uh_sum"] = graph.ndata["x"].new_zeros((number_of_nodes, 3 * self._encoder_size))
+        graph.ndata["fc_sum"] = graph.ndata["x"].new_zeros((number_of_nodes, self._encoder_size))
 
         # propagate nodes
         dgl.prop_nodes_topo(
-            graph, self._cell.message_func, self._cell.reduce_func, apply_node_func=self._cell.apply_node_func
+            graph,
+            message_func=self.message_func,
+            reduce_func=self.reduce_func,
+            apply_node_func=self.apply_node_func,
         )
 
         # [n nodes; encoder size]
-        h = self._dropout(graph.ndata.pop("h"))
+        h = graph.ndata.pop("h")
         # [n nodes; decoder size]
-        out = self._linear(h)
+        out = self._tanh(self._norm(self._out_linear(h)))
         return out
