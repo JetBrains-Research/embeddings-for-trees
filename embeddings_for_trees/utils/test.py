de9c94145c1fb2a243a0c516e7b12f33a3a4a700
Egor Spirin <spirin.egor@mail.ru>
2021-12-14 19:07:26 +0300

    Fix mypy issue

--- embeddings_for_trees/utils/test.py
+++ embeddings_for_trees/utils/test.py
@@ -4,7 +4,9 @@ import dgl
 from pytorch_lightning import LightningModule, LightningDataModule, seed_everything, Trainer
 
 
-def test(model: LightningModule, data_module: LightningDataModule, seed: Optional[int] = None, gpu: Optional = None):
+def test(
+    model: LightningModule, data_module: LightningDataModule, seed: Optional[int] = None, gpu: Optional[int] = None
+):
     seed_everything(seed)
     dgl.seed(seed)
     trainer = Trainer(gpus=gpu)
